b27040c1c76239a78ffa00273d2de0a9b7922794
Chandra Bhan Singh <cbhansingh1@gmail.com>
2022-10-30 11:09:02 +0800

    Styling for Team Page component

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,16 +1,25 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
 
+import './MatchSmallCard.scss';
 export const MatchSmallCard = ({ match, teamName }) => {
   if (!match) return null;
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/team/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
   return (
-    <div>
+    <div
+      className={
+        isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'
+      }
+    >
+      <span className='vs'>vs</span>
       <h1>
-        vs
-        <Link to={otherTeamRoute}> {otherTeam} </Link>
+        <Link to={otherTeamRoute}>{otherTeam}</Link>
       </h1>
+      <p className='match-result'>
+        {match.matchWinner} won by {match.resultMargin} {match.result}{' '}
+      </p>
     </div>
   );
 };
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,10 +1,13 @@
 import { React, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import { PieChart } from 'react-minimal-pie-chart';
+
+import './TeamPage.scss';
 
 export const TeamPage = () => {
-  const [team, setTeam] = useState();
+  const [team, setTeam] = useState({ matches: [] });
   const { teamName } = useParams();
 
   useEffect(() => {
@@ -20,13 +23,35 @@ export const TeamPage = () => {
     return <h1>Team not found</h1>;
   }
   return (
-    <div>
-      <h1>{team?.teamName}</h1>
-      <MatchDetailCard teamName={team?.teamName} match={team?.matches[0]} />
+    <div className='TeamPage'>
+      <div className='team-name-section'>
+        <h1 className='team-name'>{team.teamName}</h1>
+      </div>
+      <div className='win-loss-section'>
+        Wins / Losses
+        <PieChart
+          data={[
+            {
+              title: 'Losses',
+              value: team.totalMatches - team.totalWins,
+              color: '#a34d5d',
+            },
+            { title: 'Wins', value: team.totalWins, color: '#4da375' },
+          ]}
+        />
+      </div>
+      <div className='match-detail-section'>
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
 
-      {team?.matches?.slice(1).map((match) => (
-        <MatchSmallCard teamName={team?.teamName} match={match} />
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard key={match.id} teamName={team.teamName} match={match} />
       ))}
+
+      <div className='more-link'>
+        <Link>More</Link>
+      </div>
     </div>
   );
 };
